40c1b832dee8fa1c97d30d5802d5fb0202b0bb1c
Galushko Evgeniy <yudginchik@gmail.com>
2025-05-10 20:38:44 +0300

    add

--- firebase.js
+++ firebase.js
@@ -1,32 +1,28 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getAuth } from "firebase/auth";
 
-// console.log(process.env);
+// console.log(process.env.REACT);
 
-// const firebaseConfig = {
-//   apiKey: import.meta.env.VITE_APP_FIREBASE_APP_ID,
-//   authDomain: import.meta.env.VITE_APP_FIREBASE_AUTH_DOMAIN,
-//   databaseURL: import.meta.env.VITE_APP_FIREBASE_DATABASE_URL,
-//   projectId: import.meta.env.VITE_APP_FIREBASE_PROJECT_ID,
-//   storageBucket: import.meta.env.VITE_APP_FIREBASE_STORAGE_BUCKET,
-//   messagingSenderId: import.meta.env.VITE_APP_FIREBASE_MESSAGING_SENDER_ID,
-//   appId: import.meta.env.VITE_APP_FIREBASE_APP_ID,
-//   measurementId: import.meta.env.VITE_APP_FIREBASE_MEASUREMENT_ID,
-// };
+// const apiKey = process.env.REACT_APP_FIREBASE_APP_ID;
+// const authDomain = process.env.REACT_APP_FIREBASE_AUTH_DOMAIN;
+// const databaseURL = process.env.REACT_APP_FIREBASE_DATABASE_URL;
+// const projectId = process.env.REACT_APP_FIREBASE_PROJECT_ID;
+// const storageBucket = process.env.REACT_APP_FIREBASE_STORAGE_BUCKET;
+// const messagingSenderId = process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID;
+// const appId = process.env.REACT_APP_FIREBASE_APP_ID;
+// const measurementId = process.env.REACT_APP_FIREBASE_MEASUREMENT_ID;
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDI3r7J_fIPiX8knvZsXcoq2TJ0XxWWltU",
-  authDomain: "psychologists-services-1c5d3.firebaseapp.com",
-  databaseURL:
-    "https://psychologists-services-1c5d3-default-rtdb.firebaseio.com",
-  projectId: "psychologists-services-1c5d3",
-  storageBucket: "psychologists-services-1c5d3.firebasestorage.app",
-  messagingSenderId: "455949985564",
-  appId: "1:455949985564:web:d31b1d89f28883751c3cd9",
-  measurementId: "G-L833X56ZEQ",
+  apiKey: process.env.REACT_APP_FIREBASE_APP_ID,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
-// export const auth = getAuth(app);
-const analytics = getAnalytics(app);
+
+// const analytics = getAnalytics(app);
